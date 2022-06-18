f31c0f6513893da17de157dfe8da515efe060994
Crystal Cadbury <nabyenduojha99@gmail.com>
2022-06-19 04:20:08 +0530

    Counter added

--- script.js
+++ script.js
@@ -36,6 +36,23 @@ function sendEmail() {
 }
 
 
+// website refresh auto-counter starts
+
+var counterContainer = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view");
+
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// auto-counter ends
+
+
 
 // Go to top starts
 
